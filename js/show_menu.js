5fb436293ba3dc0d09e876d36df170a4ae93db23
cuculuis <luisangel96@gmail.com>
2023-10-22 19:55:26 -0300

    Modificaciones en el responsive y en el form, añadiendo funcionalidad al menu hamburguesa

---
+++ js/show_menu.js
@@ -0,0 +1,7 @@
+let menu = document.getElementById('menu');
+let burger = document.getElementById('burger');
+
+burger.addEventListener('click', function() {
+    menu.classList.toggle('active');
+
+})
